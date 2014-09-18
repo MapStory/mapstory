2c1f1027892f679bf92a404e4a023a70ad5d0bd2
Ian Schneider <planablediglet@gmail.com>
2014-09-18 11:46:12 -0600

    ansible - adjust prod local_settings

--- scripts/provision/files/local_settings.py
+++ scripts/provision/files/local_settings.py
@@ -1,4 +1,5 @@
 SECRET_KEY = '{{ secret_key.stdout }}'
 DATABASE_PASSWORD = '{{ pgpass }}'
 OGC_SERVER['default']['PASSWORD'] = '{{ gspass }}'
-OGC_SERVER['default']['PUBLIC_LOCATION'] = 'http://{{ nginx_server_name }}/geoserver'
+OGC_SERVER['default']['PUBLIC_LOCATION'] = 'http://{{ nginx_server_name }}/geoserver/'
+LOCAL_CONTENT = False
