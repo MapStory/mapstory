2cddcb01bce622774237e82057dce7294b5a13fc
Ian Schneider <planablediglet@gmail.com>
2014-10-08 16:30:27 -0600

    ansible - generate local key to separate file

--- scripts/provision/files/local_settings.py
+++ scripts/provision/files/local_settings.py
@@ -1,4 +1,4 @@
-SECRET_KEY = '{{ secret_key.stdout }}'
+from .local_key import *
 DATABASE_PASSWORD = '{{ pgpass }}'
 OGC_SERVER['default']['PASSWORD'] = '{{ gspass }}'
 OGC_SERVER['default']['PUBLIC_LOCATION'] = 'http://{{ nginx_server_name }}/geoserver/'
