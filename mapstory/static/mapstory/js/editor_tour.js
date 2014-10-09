0c34d12aa9abd822fdd9a386f5d7b78a94ab7f50
Ian Schneider <planablediglet@gmail.com>
2014-10-09 17:03:03 -0600

    stub out hopscotch editor tour

---
+++ mapstory/static/mapstory/js/editor_tour.js
@@ -0,0 +1,29 @@
+editor_tour = {
+    id: "welcome_tour",
+    steps: [
+        {
+            target: "ms-tl-nav",
+            placement: "bottom",
+            title: "This is the navigation menu",
+            content: "Use the links here to get around on our site!"
+        },
+        {
+            target: "search-panel",
+            placement: "right",
+            title: "Search for something",
+            content: "Not actually sure what this searches"
+        },
+        {
+            target: "layer-manager-panel",
+            placement: "bottom",
+            title: "Manage Layers",
+            content: "Add, remove or edit layers in the map"
+        },
+        {
+            target: $("a[href=#ms-toggle-preview]").get(0),
+            placement: "bottom",
+            title: "Press this",
+            content: "To do more stuff"
+        }
+    ]
+};
