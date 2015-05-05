a4904b0a13d180b0a394a37764cdca7926692910
Ian Schneider <planablediglet@gmail.com>
2015-05-05 16:23:13 -0600

    add help_text link re textile formatting
    refs #19

--- mapstory/models.py
+++ mapstory/models.py
@@ -42,7 +42,10 @@ class Sponsor(models.Model):
 
 
 class ContentMixin(models.Model):
-    content = models.TextField()
+    content = models.TextField(
+        help_text="use <a href=%s target='_'>textile</a> for the content" %
+        'http://redcloth.org/hobix.com/textile/'
+    )
     date = models.DateTimeField(default=datetime.now)
     publish = models.BooleanField(default=False)
 
