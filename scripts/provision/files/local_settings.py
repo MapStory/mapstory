c7bc5a0a57529f1fc13fafffd3b1dbf94b7698cd
Ian Schneider <planablediglet@gmail.com>
2015-05-08 12:24:55 -0600

    deployment - fix media root configuration
    fixes #20

--- scripts/provision/files/local_settings.py
+++ scripts/provision/files/local_settings.py
@@ -1,4 +1,5 @@
 from .local_key import *
+MEDIA_ROOT = '{{media_root}}'
 DATABASE_PASSWORD = '{{ pgpass }}'
 OGC_SERVER['default']['PASSWORD'] = '{{ gspass }}'
 OGC_SERVER['default']['PUBLIC_LOCATION'] = 'http://{{ nginx_server_name }}/geoserver/'
