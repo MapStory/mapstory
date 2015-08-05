959e9cd61fe20cf82421006f5d55698f1e0063c5
travis <travislbrundage@gmail.com>
2015-08-04 22:26:55 -0700

    Fixed counters on profile page

--- mapstory/static/mapstory/js/search.js
+++ mapstory/static/mapstory/js/search.js
@@ -179,23 +179,19 @@
 
     // Here we want to grab the number of layers and number of maps
     // query={q: query.q}; query.type__in='map'; search();
-    $scope.calculate_layers = function() {
+    $scope.calculate_maps_layers = function() {
       $scope.query.type__in = 'layer';
       $scope.search().then(function(result) {
         $scope.total_layers = $scope.total_counts;
-        $scope.query.type__in = null;
+        $scope.query.type__in = 'map';
+        $scope.search().then(function(result) {
+          $scope.total_maps = $scope.total_counts;
+          $scope.query.type__in = null;
+        });
       });
     };
 
-    $scope.calculate_maps = function() {
-      $scope.query.type__in = 'map';
-      $scope.search().then(function(result) {
-        $scope.total_maps = $scope.total_counts;
-        $scope.query.type__in = null;
-      });
-    };
-    $scope.calculate_layers();
-    $scope.calculate_maps();
+    $scope.calculate_maps_layers();
 
     $scope.showUserGroup = function() {
         if ($location.search().hasOwnProperty('type__in')) {
