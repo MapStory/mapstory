7ed611cbc59295e270d1e505929579e20abac905
Tyler Garner <garnertb@gmail.com>
2015-10-13 10:07:20 -0400

    Add tests for the StoryBoxes app.

---
+++ mapstory/apps/boxes/tests.py
@@ -0,0 +1,137 @@
+import datetime
+import json
+from mapstory.tests import MapStoryTestMixin, AdminClient
+from geonode.base.models import TopicCategory
+from geonode.base.populate_test_data import create_models
+from geonode.maps.models import Map
+from django.core.urlresolvers import reverse
+from .forms import StoryBoxForm
+from .utils import parse_date_time, datetime_to_seconds, make_point
+from .models import StoryBox
+
+
+class MapStoryTestsWorkFlowTests(MapStoryTestMixin):
+
+    def setUp(self):
+        # these are needed for the geonode fixtures
+        TopicCategory.objects.create(identifier='biota')
+        TopicCategory.objects.create(identifier='location')
+        TopicCategory.objects.create(identifier='elevation')
+
+        create_models(type='map')
+
+    def test_box_form(self):
+        data = {}
+        m = Map.objects.first()
+        form = StoryBoxForm(data=data)
+        self.assertFalse(form.is_valid())
+        self.assertTrue('map' in form.errors.keys())
+        self.assertTrue('title' in form.errors.keys())
+
+        data['map'] = m.id
+        data['title'] = 'this is a test'
+
+        form = StoryBoxForm(data=data)
+        self.assertTrue(form.is_valid())
+
+        data['geometry'] = make_point(-77.464599609375, 37.61423141542417)
+
+        form = StoryBoxForm(data=data)
+        self.assertTrue(form.is_valid())
+
+    def test_box_form_parse_float(self):
+        """
+        Tests the parse float method on the box form.
+        """
+        form = StoryBoxForm(data={'test': '1234.234'})
+        self.assertEqual(form.parse_float('test'), 1234.234)
+
+        self.assertIsNone(form.parse_float('nope'))
+
+        # ensure value errors get added to the form.errors dict
+        form = StoryBoxForm(data={'lat': 'nope'})
+        form.full_clean()
+        self.assertTrue('lat' in form.errors)
+
+    def test_parse_date_time(self):
+        """
+        Tests the parse date time function.
+        """
+
+        self.assertIsNone(parse_date_time(None))
+
+        with self.assertRaises(ValueError):
+            self.assertIsNone(parse_date_time('-2015'))
+
+        self.assertEqual(parse_date_time('2015/2'), datetime.datetime(2015, 2, 1, 0, 0))
+        self.assertEqual(parse_date_time('2015/2/6'), datetime.datetime(2015, 2, 6, 0, 0))
+        self.assertEqual(parse_date_time('2015/2/6T14'), datetime.datetime(2015, 2, 6, 14, 0))
+        self.assertEqual(parse_date_time('2015/2/6 14'), datetime.datetime(2015, 2, 6, 14, 0))
+        self.assertEqual(parse_date_time('2015/2/6T14:59'), datetime.datetime(2015, 2, 6, 14, 59))
+        self.assertEqual(parse_date_time('2015/2/6 14:59'), datetime.datetime(2015, 2, 6, 14, 59))
+        self.assertEqual(parse_date_time('2015/2/6T14:59:59'), datetime.datetime(2015, 2, 6, 14, 59, 59))
+        self.assertEqual(parse_date_time('2015/2/6 14:59:59'), datetime.datetime(2015, 2, 6, 14, 59, 59))
+
+        self.assertEqual(parse_date_time('2015-2'), datetime.datetime(2015, 2, 1, 0, 0))
+        self.assertEqual(parse_date_time('2015-2-6'), datetime.datetime(2015, 2, 6, 0, 0))
+        self.assertEqual(parse_date_time('2015-2-6T14'), datetime.datetime(2015, 2, 6, 14, 0))
+        self.assertEqual(parse_date_time('2015-2-6 14'), datetime.datetime(2015, 2, 6, 14, 0))
+        self.assertEqual(parse_date_time('2015-2-6T14:59'), datetime.datetime(2015, 2, 6, 14, 59))
+        self.assertEqual(parse_date_time('2015-2-6 14:59'), datetime.datetime(2015, 2, 6, 14, 59))
+        self.assertEqual(parse_date_time('2015-2-6T14:59:59'), datetime.datetime(2015, 2, 6, 14, 59, 59))
+        self.assertEqual(parse_date_time('2015-2-6 14:59:59'), datetime.datetime(2015, 2, 6, 14, 59, 59))
+
+    def test_datetime_to_seconds(self):
+        """
+        Tests the datetime to seconds function.
+        """
+
+        self.assertEqual(datetime_to_seconds(datetime.datetime(2009, 10, 21, 0, 0)), 1256083200)
+
+    def test_boxes_view(self):
+        """
+        Tests various methods of the boxes view.
+        """
+
+        m = Map.objects.first()
+
+        c = AdminClient()
+        response = c.get(reverse('boxes', args=[m.id]))
+        self.assertEqual(response.status_code, 200)
+
+        response = c.delete(reverse('boxes', args=[m.id]))
+        self.assertEqual(response.status_code, 400)
+
+        response = c.put(reverse('boxes', args=[m.id]))
+        self.assertEqual(response.status_code, 400)
+
+        data = dict(map=m.id, title='this is a test', geometry=make_point(-77.464599609375, 37.61423141542417))
+        response = c.post(reverse('boxes', args=[m.id]), data=data)
+        self.assertEqual(response.status_code, 403)
+
+        # TODO: Test POST request with box payload
+        # c.login_as_admin()
+        #
+        # data['geometry'] = {
+        #       "type": "FeatureCollection",
+        #       "features": [
+        #         {
+        #           "type": "Feature",
+        #           "properties": {},
+        #           "geometry": {
+        #             "type": "Point",
+        #             "coordinates": [
+        #               -77.464599609375,
+        #               37.61423141542417
+        #             ]
+        #           }
+        #         }
+        #       ]
+        #     }
+        #
+        # response = c.post(reverse('boxes', args=[m.id]), data=json.dumps(data), content_type='application/json')
+        # self.assertEqual(response.status_code, 200)
+        #
+        # box = StoryBox.objects.first()
+        # self.assertEqual(box.title, data['title'])
+        # self.assertEqual(box.map, m)
