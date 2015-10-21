112e87487124d7330d19e42001780c71f6fc146d
Clarence Davis Jr <cdavis@boundlessgeo.com>
2015-10-21 16:28:55 -0500

    Added django-oauth2-provider and vars for social auth

--- mapstory/urls.py
+++ mapstory/urls.py
@@ -114,7 +114,11 @@ if settings.DEBUG:
         url(r'^testing/(?P<template>.*)$', 'mapstory.views.test_view'),
     )
 
-
 if settings.LOCAL_CONTENT:
     urlpatterns = static(settings.STATIC_URL + "assets", document_root=settings.LOCAL_ROOT + "/../../mapstory-assets", show_indexes=True) + urlpatterns
 
+if settings.ENABLE_SOCIAL_LOGIN:
+    urlpatterns += patterns('',
+        url(r'', include('social_auth.urls')),
+        url(r'^oauth2/', include('provider.oauth2.urls', namespace = 'oauth2')),
+    )
