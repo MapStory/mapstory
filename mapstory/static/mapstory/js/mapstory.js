8f1ff2f0e30d2ed2f2144904858c5d6314c4a6f6
Tyler Garner <garnertb@gmail.com>
2016-01-21 08:28:19 -0500

    Modalize the add layer functionality.

--- mapstory/static/mapstory/js/mapstory.js
+++ mapstory/static/mapstory/js/mapstory.js
@@ -1,7 +1,155 @@
 'use strict';
 
 (function() {
-    angular.module('mapstory', [
-      'mapstory.uploader'
-  ]);
+    angular.module('mapstory', [ 'mapstory.uploader', 'ui.bootstrap', 'geonode_main_search', 'leaflet-directive'], function($locationProvider) {
+         if (window.navigator.userAgent.indexOf("MSIE") == -1){
+          $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+          });
+
+          // make sure that angular doesn't intercept the page links
+          angular.element("a").prop("target", "_self");
+      }
+    })
+    .constant('Configs', {
+    url: SEARCH_URL
+    })
+    .controller('createLayerCtrl', function($scope, $uibModal) {
+      $scope.open = function (templateUrl, modalImage, staticUrl) {
+          var modalInstance = $uibModal.open({
+            animation: true,
+            templateUrl: templateUrl || 'importWizard.html',
+            controller:  'createLayerModalCtrl',
+            resolve: {
+              modalImage: function () {
+                return modalImage;
+              },
+              staticUrl: function () {
+                return staticUrl;
+              }
+            }
+          });
+
+          modalInstance.result.then(function (selectedItem) {
+            $scope.selected = selectedItem;
+          }, function () {
+            console.log('Modal dismissed at: ' + new Date());
+          });
+        };
+    })
+    .controller('createLayerModalCtrl', function($scope, $modalInstance) {
+      $scope.ok = function () {
+        $modalInstance.dismiss('cancel');
+      };
+
+      $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+      };
+    })
+.controller('search_controller', function($injector, $scope, $location, $http){
+    $scope.query = $location.search();
+    $scope.query.limit = $scope.query.limit || CLIENT_RESULTS_LIMIT;
+    $scope.query.offset = $scope.query.offset || 0;
+    $scope.page = Math.round(($scope.query.offset / $scope.query.limit) + 1);
+
+    $scope.search = function() {
+      $scope.query.limit = 100;
+      $scope.query.offset = 0;
+      return query_api($scope.query).then(function(result) {
+        return result;
+      });
+    };
+
+    //Get data from apis and make them available to the page
+    function query_api(data){
+      return $http.get('/api/base/search/', {params: data || {}}).success(function(data){
+        $scope.results = data.objects;
+        $scope.total_counts = data.meta.total_count;
+        $scope.$root.query_data = data;
+        if (HAYSTACK_SEARCH) {
+          if ($location.search().hasOwnProperty('q')){
+            $scope.text_query = $location.search()['q'].replace(/\W+/g," ");
+          }
+          if ($location.search().hasOwnProperty('type__in')){
+            $scope.type__in = $location.search()['type__in'].replace(/\W+/g," ");;
+          }
+        } else {
+          if ($location.search().hasOwnProperty('title__icontains')){
+            $scope.text_query = $location.search()['title__icontains'].replace(/\W+/g," ");
+          }
+        }
+
+        //Update facet/keyword/category counts from search results
+        if (HAYSTACK_FACET_COUNTS){
+            module.haystack_facets($http, $scope.$root, $location);
+            $("#types").find("a").each(function(){
+                if ($(this)[0].id in data.meta.facets.subtype) {
+                    $(this).find("span").text(data.meta.facets.subtype[$(this)[0].id]);
+                }
+                else if ($(this)[0].id in data.meta.facets.type) {
+                    $(this).find("span").text(data.meta.facets.type[$(this)[0].id]);
+                } else {
+                    $(this).find("span").text("0");
+                }
+            });
+        }
+
+        // Initialize carousel display
+        $scope.display = [];
+        $scope.indeces = [];
+        for (var i = 0; i < $scope.results.length; i++) {
+            $scope.display[i] = $scope.results[i];
+            $scope.indeces[i] = i;
+            if (i >= 3) {
+                break;
+            }
+        }
+      });
+    };
+
+    $scope.query['is_published'] = true;
+    $scope.query['featured'] = true;
+    query_api($scope.query);
+
+    $scope.query_category = function(category, type) {
+      $scope.query.type__in = type;
+      $scope.query.category__identifier__in = category;
+      $scope.search();
+    };
+
+    // carousel
+    $scope.slideLeft = function() {
+        for (var i = 0; i < $scope.indeces.length; i++) {
+            $scope.indeces[i] = ($scope.indeces[i] + 1) % $scope.results.length;
+        }
+        $scope.updateDisplay();
+    };
+
+    $scope.slideRight = function() {
+        for (var i = 0; i < $scope.indeces.length; i++) {
+            $scope.indeces[i] = ($scope.indeces[i] - 1 + $scope.results.length) % $scope.results.length;
+        }
+        $scope.updateDisplay();
+    };
+
+    $scope.updateDisplay = function() {
+        for (var i = 0; i < $scope.indeces.length; i++) {
+            $scope.display[i] = $scope.results[$scope.indeces[i]];
+        };
+      }
+    })
+
+// this is from geonode it is at least used on the search page.
+.controller('leaflet_hack', function($scope, leafletData) {
+    $('#regions').on('shown.bs.collapse', function() {
+        leafletData.getMap().then(function(map) {
+            map.invalidateSize();
+        });
+    });
+  });
+
+    angular.element(document).ready(function() {
+      angular.bootstrap(document, ['mapstory']);
+    });
 })();
