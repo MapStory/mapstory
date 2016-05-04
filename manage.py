3d81af9ea10073ecd32c85e88db52a09f7312617
Jorge Martinez <jorgemg0327@gmail.com>
2016-05-04 11:17:23 -0500

    Replacing mapstory name for storyscape in templates

--- manage.py
+++ manage.py
@@ -7,6 +7,6 @@ import sys
 if __name__ == "__main__":
     from django.core.management import execute_from_command_line
 
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mapstory.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mapstory.settings.sc_settings")
     execute_from_command_line(sys.argv)
 
---
+++ mapstory/settings/sc_settings.py
@@ -0,0 +1,5 @@
+from mapstory.settings import *
+
+TEMPLATE_DIRS = (
+    os.path.join(LOCAL_ROOT, "templates_sc"),
+) + TEMPLATE_DIRS
