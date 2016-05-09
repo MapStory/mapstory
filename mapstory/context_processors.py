bc68180d85de81fd1265a3f76342474ffa21348f
Clarence Davis Jr <cldavisjr@gmail.com>
2016-05-09 11:55:16 -0500

    Allow current site to be accessible from a template

--- mapstory/context_processors.py
+++ mapstory/context_processors.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 
 from .context_processors_favorite import get_favorite_info
+from django.contrib.sites.models import Site
 
 
 def context(req):
@@ -9,5 +10,6 @@ def context(req):
         favorite_info=get_favorite_info(req),
         GOOGLE_ANALYTICS=getattr(settings, 'GOOGLE_ANALYTICS', None),
         ENABLE_SOCIAL_LOGIN=getattr(settings, 'ENABLE_SOCIAL_LOGIN', False),
-        USER_SNAP=getattr(settings, 'USER_SNAP', False)
+        USER_SNAP=getattr(settings, 'USER_SNAP', False),
+        site=Site.objects.get_current()
     )
