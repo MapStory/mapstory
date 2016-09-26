effaac08c6459be8503b6a9b17deee684dfc1bee
John Wileczek <john.wileczek@gmail.com>
2016-09-26 13:37:09 -0400

    Minor output improvements to command and laundering old name in case it contains special characters that would mess up links.

--- mapstory/management/commands/convert_old_stories.py
+++ mapstory/management/commands/convert_old_stories.py
@@ -1,9 +1,15 @@
 from django.core.management.base import BaseCommand, CommandError
 from geonode.maps.models import Map,MapStory
+import re
 
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
+        # private function to launder story name in case it contains special characters
+        def launder(string):
+
+            return re.sub('[^0-9a-zA-Z]+', '_', string.lower())
+
         old_stories = Map.objects.filter(story__isnull=True)
         stories_updated = 0
 
@@ -11,20 +17,21 @@ class Command(BaseCommand):
 
             for old_story in old_stories.iterator():
 
-                #create the new story model object, populating it with data from old map in some cases.
+                # create the new story model object, populating it with data from old map in some cases.
                 new_story = MapStory(owner=old_story.owner,
                                      title=old_story.title,
-                                     name=old_story.name,
+                                     name=launder(old_story.name),
                                      abstract=old_story.abstract)
                 new_story.save()
                 new_story.set_default_permissions()
 
-                #create the foreign key link to the new story and set it to the first chapter
+                # create the foreign key link to the new story and set it to the first chapter
                 old_story.story = new_story
                 old_story.chapter_index = 0
                 old_story.save()
 
                 stories_updated += 1
+                self.stdout.write('Converted old mapstory: {0}'.format(old_story.name))
 
             self.stdout.write('{0} stories converted to new model'.format(stories_updated))
 
