e442595a25203b10e684090b120ece88e5b6cd30
emilyashley <ohemilyashley@gmail.com>
2016-12-15 16:04:35 -0600

    Remove unused django tags from rendering

--- mapstory/templatetags/mapstory_tags.py
+++ mapstory/templatetags/mapstory_tags.py
@@ -9,11 +9,9 @@ register = template.Library()
 def remote_content(path):
     return '%s/%s' % (settings.REMOTE_CONTENT_URL, path)
 
-
 @register.simple_tag
 def link(href, name, width=None, height=None, css_class=None):
     return Link(href, name).render(width, height, css_class)
-
 
 @register.filter
 def by_name(objects, name):
