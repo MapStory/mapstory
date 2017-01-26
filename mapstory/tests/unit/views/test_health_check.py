d269f89985668b8a929f43cb0e64829b9f70db79
Zunware <jose@zunware.com>
2017-01-26 13:45:57 -0600

    Fixed auth problem with health_check test

--- mapstory/tests/unit/views/test_health_check.py
+++ mapstory/tests/unit/views/test_health_check.py
@@ -13,7 +13,7 @@ class HealthCheckViewTest(MapStoryTestMixin):
         pass
     
     def test_responses(self):
-        self.userclient.login_as_non_admin(username=self.test_username, password=self.test_password)
+        self.userclient.login_as_admin()
         response = self.userclient.get(reverse('health_check'))
         self.assertTemplateUsed(response, 'health_check/dashboard.html')
 
