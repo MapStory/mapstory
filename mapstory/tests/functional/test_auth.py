9bfec86914a787119be6a69adefabe70c5db0078
Zunware <jose@zunware.com>
2017-02-13 12:42:26 -0600

    Added auth test scenarios

--- mapstory/tests/functional/test_auth.py
+++ mapstory/tests/functional/test_auth.py
@@ -24,5 +24,59 @@ from django.contrib.sites.models import Site
 from ..AdminClient import AdminClient
 
 class AuthFunctionalTests(TestCase):
-    def test_create_user(self):
+    def test_signup_correct_form(self):
+    # Given:
+        # - User is not logged in
+        # - We are on home page
+        # - Signup form is visible
+    # When:
+        # - The user fills in correctly the signup form
+    # Then:
+        # - Will receive confirmation of success
+        pass
+
+    def test_sigunp_incorrect_email(self):
+    # Given:
+        # - User is not logged in
+        # - We are on home page
+        # - Signup form is visible
+    # When:
+        # - The user does not give a valid email
+    # Then:
+        # - Will receive an error message
+        pass
+
+    def test_sigunp_incorrect_terms_and_conditions(self):
+    # Given:
+        # - User is not logged in
+        # - We are on home page
+        # - Signup form is visible
+    # When:
+        # - The user does not consent to terms and conditions
+    # Then:
+        # - Will receive an error message
+        pass
+
+    def test_correct_login(self):
+    # Given:
+        # - The user exists
+        # - User is not logged in
+        # - We are on home page
+        # - Login form is visible
+    # When:
+        # - The user provides correct credentials
+    # Then:
+        # - Will be authorized
+        pass
+
+    def test_incorrect_login(self):
+    # Given:
+        # - The user exists
+        # - User is not logged in
+        # - We are on home page
+        # - Login form is visible
+    # When:
+        # - The user provides incorrect credentials
+    # Then:
+        # - Will receive error
         pass
