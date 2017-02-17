dfad9046426ec69ed0dfaeb5f909f13b9fe308b1
Sara Safavi <sara@sarasafavi.com>
2017-02-17 15:20:05 -0600

    add migrations originally from geonode.maps (in fork)

---
+++ mapstory/mapstories/migrations/0003_map_viewer_playbackmode.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('mapstories', '0002_add_map_chapters'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='map',
+            name='viewer_playbackmode',
+            field=models.CharField(max_length=32, null=True, verbose_name='Viewer Playback', blank=True),
+        ),
+    ]
