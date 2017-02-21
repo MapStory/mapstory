0d4bff84db9f711400f3ae0e1f04ccc40b892123
Jeremiah Cooper <JCooper@jeremiahcooper.com>
2017-02-21 12:29:48 -0500

    This commit resolves issues #446 and #447 on Github.  I’ve changed the fullname variable that we return to the /layers/acls and /layers/resolve_user API endpoints because geoserver-geonode extension uses the fullname rather than the username of a user.  This data then gets passed from geoserver-geonode-ext to geoserver to geogig.  This allows us to use the django username of the user for all commit related information.

--- mapstory/urls.py
+++ mapstory/urls.py
@@ -29,6 +29,7 @@ from mapstory.views import organization_create, organization_edit, organization_
 from mapstory.views import organization_invite, organization_members_add, organization_member_remove
 from mapstory.views import initiative_create, initiative_edit, initiative_detail, initiative_members
 from mapstory.views import initiative_invite, initiative_members_add, initiative_member_remove
+from mapstory.views import layer_acls_mapstory, resolve_user_mapstory
 from tastypie.api import Api
 from mapstory.api import UploadedLayerResource
 from annotations.urls import urlpatterns as annotations_urls
@@ -125,6 +126,8 @@ urlpatterns = patterns('',
     url(r'^layers/notify-download$', notify_download, name='notify-layer-download'),
     url(r'^layers/download-append-csv$', download_append_csv, name='download_append_csv'),
     url(r'^layers/download-append-shp$', download_append_shp, name='download_append_shp'),
+    url(r'^layers/acls', layer_acls_mapstory, name='layer_acls_mapstory'),
+    url(r'^layers/resolve_user', resolve_user_mapstory, name='resolve_user_mapstory'),
 
 ) + geonode_layers_urlpatterns + layer_detail_patterns + urlpatterns
 
