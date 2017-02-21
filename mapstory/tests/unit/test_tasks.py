9542fbca32a22eb8209bad035e66735278984a80
Zunware <jose@zunware.com>
2017-02-21 01:01:47 -0600

    Installed mock Added notifications tests Added tasks tests

---
+++ mapstory/tests/unit/test_tasks.py
@@ -0,0 +1,26 @@
+from unittest import skip
+
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse
+from django.test import TestCase
+
+from ...tests.MapStoryTestMixin import MapStoryTestMixin
+from ...tests.AdminClient import AdminClient
+from ...utils import parse_schema, error_response, Link
+
+from mapstory.tasks import append_feature_chunks
+
+class WFST_Template(object):
+    def format(self, features, workspace, workspace_uri, handle):
+        pass
+
+
+class TestTasks(MapStoryTestMixin):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_append_feature_chunks(self):
+        append_feature_chunks(['a','b','c'], WFST_Template(), "A", "B")
