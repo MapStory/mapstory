104257a6239ee6cf117dd6aa2dc56fddd9122c74
Zunware <jose@zunware.com>
2017-02-27 13:40:32 -0600

    Configured for automated browser testing on saucelabs

--- mapstory/tests/e2e/conf.js
+++ mapstory/tests/e2e/conf.js
@@ -10,14 +10,64 @@
  *
  */
 
+// Default configuration:
+let selenium_url = 'http://localhost:4444/wd/hub';
+let multi_cabapilities = [{
+	'browserName' : 'chrome',
+	'chromeOptions': {
+		// Get rid of --ignore-certificate yellow warning
+		args: ['--no-sandbox', '--test-type=browser'],
+		// Set download path and avoid prompting for download even though
+		// this is already the default on Chrome but for completeness
+		prefs: {
+			'download': {
+				'prompt_for_download': false,
+				'default_directory': '../downloads/'
+			}
+		}
+	}}
+];
+
+
+// This overrides the configuration if we are running inside Travis
+if(process.env.TRAVIS) {
+	// Use sauce labs for cloud browser testing
+	selenium_url = 'http://' + process.env.SAUCE_USERNAME + ':' + process.env.SAUCE_ACCESS_KEY +'@ondemand.saucelabs.com/wd/hub';
+
+	// Set the browsers we want to test on
+	multi_cabapilities = [{
+		'browserName' : 'firefox',
+		'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+		'name': 'Mapstory Firefox Tests',
+		'build': process.env.TRAVIS_BUILD_NUMBER,
+		'tags': [process.env.TRAVIS_PYTHON_VERSION, 'CI']
+	},{
+		'browserName' : 'chrome',
+		'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+		'build': process.env.TRAVIS_BUILD_NUMBER,
+		'tags': [process.env.TRAVIS_PYTHON_VERSION, 'CI'],
+		'name': 'Mapstory Chrome Tests',
+		'chromeOptions': {
+			// Get rid of --ignore-certificate yellow warning
+			args: ['--no-sandbox', '--test-type=browser'],
+			// Set download path and avoid prompting for download even though
+			// this is already the default on Chrome but for completeness
+			prefs: {
+				'download': {
+					'prompt_for_download': false,
+					'default_directory': '../downloads/'
+				}
+			}
+		}
+	}];
+}
+
 exports.config = {
 	framework: 'jasmine',
-	seleniumAddress: 'http://localhost:4444/wd/hub',
-
+	seleniumAddress: selenium_url,
 	//--------------------------
 	// Use this to run all test files
 	specs: ['specs/*.spec.js'],
-
 	//--------------------------
 	// Use this to cherry-pick tests
 	/*specs: [
@@ -31,37 +81,17 @@ exports.config = {
 		'journal.spec.js'
 	],
 	*/
-
 	//---------------------------------------
 	// Use this to run the tests in several browsers simultaniously
-	multiCapabilities: [
-		{
-			'browserName' : 'chrome',
-			'chromeOptions': {
-				// Get rid of --ignore-certificate yellow warning
-				args: ['--no-sandbox', '--test-type=browser'],
-				// Set download path and avoid prompting for download even though
-				// this is already the default on Chrome but for completeness
-				prefs: {
-					'download': {
-						'prompt_for_download': false,
-						'default_directory': '../downloads/'
-					}
-				}
-			}
-		}
-		//-----------------------------
-		// Uncomment to use:
-		// {'browserName' : 'firefox'}
-		// {'browserName' : 'safari'},
-	],
-
+	multiCapabilities: multi_cabapilities,
 	jasmineNodeOpts: {
 		showColors: true, // Use colors in the command line report.
-		defaultTimeoutInterval: 30000
+		defaultTimeoutInterval: 3000
 	},
-	allScriptsTimeout: 30000,
-	//----------------------------------------------
+	allScriptsTimeout: 3000,
 	// Results output file
-	resultJsonOutputFile:'./result.json'
+	resultJsonOutputFile:'./result.json',
+	// SauceLabs Config
+	sauceUser: process.env.SAUCE_USERNAME,
+	sauceKey: process.env.SAUCE_ACCESS_KEY
 };
