4f73308d1f33690889824db44219b270c42ba64d
Glynnis Ritchie <gritchie@boundlessgeo.com>
2017-03-17 14:51:28 -0500

    Fix tests locally

--- mapstory/tests/unit/forms/test_Organizations.py
+++ mapstory/tests/unit/forms/test_Organizations.py
@@ -25,5 +25,5 @@ class TestOrganizations(MapStoryTestMixin):
         soup = BeautifulSoup(response.content)
 
         # Should have 28 fields total
-        self.assertEqual(len(soup.find_all('input')), 27)
+        self.assertEqual(len(soup.find_all('input')), 28)
 
