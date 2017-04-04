772cb13be26d98da1e48f7cdb397641292d8d65c
emilyashley <ohemilyashley@gmail.com>
2017-04-03 22:46:25 -0500

    Removing jqueryUI & bs-tokenfield from site in lieu of Angular Material.
    Hiding tokenfield from organization page and collection controller until we init chips there.

--- mapstory/static/mapstory/js/src/detail.controller.js
+++ mapstory/static/mapstory/js/src/detail.controller.js
@@ -8,7 +8,43 @@
     .module('mapstory')
     .controller('detailController', detailController);
 
-  function detailController($scope, $http){
+  function detailController($scope, $http, $mdConstant){
+    $scope.chips = keywords;
+    $scope.readOnly = loggedIn === "True" ? false : true;
+    $scope.separateOn = [
+      $mdConstant.KEY_CODE.ENTER, 
+      $mdConstant.KEY_CODE.COMMA, 
+      $mdConstant.KEY_CODE.SEMICOLON, 
+      $mdConstant.KEY_CODE.SPACE
+    ];
+
+    $scope.showShare = function(){
+      $scope.sharing = $scope.sharing ? !$scope.sharing : true;
+    }
+
+    $scope.addTag = function(chip){
+      $.ajax({
+        url: url,
+        type:'POST',
+        data:{ 
+          add_keyword: chip
+        }
+      });
+    };
+
+    $scope.removeTag = function(chip){
+      $.ajax({
+        url: url,
+        type:'POST',
+        data:{ 
+          remove_keyword: chip
+        }
+      });
+    };
+
+    $scope.newChip = function(chip){
+      return chip.toLowerCase();
+    };
 
     $("#comment_submit_btn").click(function (event) {
       $.ajax({
@@ -24,82 +60,6 @@
         }
       });
       return false;
-    });
-
-    $scope.showShare = function(){
-      $scope.sharing = $scope.sharing ? !$scope.sharing : true;
-    }
-
-    $scope.tags = keywords;
-
-    var updatePlaceholder = function(){
-      var textField = $('#tokenfield-tags-tokenfield');
-
-      if ($scope.tags.length > 0){
-        textField.prop('placeholder', '');
-      }else{
-        textField.prop('placeholder', 'Type here to add tags...');
-      }
-    }
-
-    // Manually set the value field
-    var value = $('#tokenfield-tags').val($scope.tags);
-    // Only initialize the tokenfield once the values are set
-    if (value) {
-      $('#tokenfield-tags').tokenfield()
-      .on('tokenfield:createtoken', function(e) {
-        var data = e.attrs.value.split(' ');
-        // Create token for first word
-        e.attrs.value = data[0];
-        e.attrs.label = data[0];
-        // Create tokens for remaining words, if any, separated by spaces
-        for (var i = 1; i < data.length; i++) {
-          $('#tokenfield-tags').tokenfield('createToken', data[i]);
-        }
-      })
-      .on('tokenfield:createdtoken', function(e) {
-        if ($scope.tags.indexOf(e.attrs.value) == -1) {
-          $scope.tags.push(e.attrs.value);
-          updatePlaceholder();
-
-          $.ajax({
-            url: url,
-            type:'POST',
-            data:{ 
-              add_keyword: e.attrs.value.toLowerCase()
-            }
-          });
-        }
-      })
-      .on('tokenfield:removedtoken', function(e) {
-        var index = $scope.tags.indexOf(e.attrs.value);
-        if (index > -1) {
-          $scope.tags.splice(index, 1);
-          updatePlaceholder();
-
-          $.ajax({
-            url: url,
-            type:'POST',
-            data: {
-              remove_keyword: e.attrs.value
-            }
-          });
-        }
-      });
-
-      //After initializing token field adjust placeholder text
-      updatePlaceholder();
-    }
-    // Manually set the value field
-    var value_ro = $('#tokenfield-tags-readonly').val($scope.tags);
-    if (value_ro) {
-      $('#tokenfield-tags-readonly').tokenfield();
-      $('#tokenfield-tags-readonly').tokenfield('readonly');
-    }
-    // If a label is clicked, do a manual redirect to the explore page with the value of the token as the keyword search filter
-    $('.token-label').click(function(e) {
-      var tag = $(e.target).text();
-      window.location.href = '/search/?limit=100&offset=0&keywords__slug__in=' + tag;
     });
   }
 })();
