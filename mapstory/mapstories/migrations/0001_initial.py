c983549e4992a627a68449a8aec22adb2a161e2f
Sara Safavi <sara@sarasafavi.com>
2017-04-12 09:34:59 -0500

    simplify migrations for new "mapstories" model

--- mapstory/mapstories/migrations/0001_initial.py
+++ mapstory/mapstories/migrations/0001_initial.py
@@ -1,118 +1,46 @@
 # -*- coding: utf-8 -*-
-#########################################################################
-#
-# Copyright (C) 2016 OSGeo
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
-#########################################################################
-
 from __future__ import unicode_literals
 
 from django.db import migrations, models
-from django.conf import settings
-import geonode.utils
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('base', '24_initial'),
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('notification', '0001_initial'),
-        ('mapstory_profile', '0001_initial'),
+        ('base', '__first__'),
+        ('maps', '24_initial'),
     ]
 
     operations = [
         migrations.CreateModel(
             name='Map',
             fields=[
-                ('resourcebase_ptr',
-                    models.OneToOneField(
-                        parent_link=True, auto_created=True, primary_key=True, serialize=False,
-                        to='base.ResourceBase')),
-                ('title_en',
-                    models.CharField(
-                        help_text='name by which the cited resource is known', max_length=255, null=True,
-                        verbose_name='title')),
-                ('abstract_en',
-                    models.TextField(
-                        help_text='brief narrative summary of the content of the resource(s)', null=True,
-                        verbose_name='abstract', blank=True)),
-                ('purpose_en',
-                    models.TextField(
-                        help_text='summary of the intentions with which the resource(s) was developed', null=True,
-                        verbose_name='purpose', blank=True)),
-                ('constraints_other_en',
-                    models.TextField(
-                        help_text='other restrictions and legal prerequisites for accessing and using '
-                                  'the resource or metadata', null=True,
-                        verbose_name='restrictions other', blank=True)),
-                ('supplemental_information_en',
-                    models.TextField(
-                        default='No information provided',
-                        help_text='any other descriptive information about the dataset', null=True,
-                        verbose_name='supplemental information')),
-                ('data_quality_statement_en',
-                    models.TextField(
-                        help_text="general explanation of the data producer's knowledge about the lineage of a dataset",
-                        null=True, verbose_name='data quality statement', blank=True)),
-                ('zoom', models.IntegerField(verbose_name='zoom')),
-                ('projection', models.CharField(max_length=32, verbose_name='projection')),
-                ('center_x', models.FloatField(verbose_name='center X')),
-                ('center_y', models.FloatField(verbose_name='center Y')),
-                ('last_modified', models.DateTimeField(auto_now_add=True)),
-                ('urlsuffix', models.CharField(max_length=255, verbose_name='Site URL', blank=True)),
-                ('featuredurl', models.CharField(max_length=255, verbose_name='Featured Map URL', blank=True)),
+                ('map_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='maps.Map')),
+                ('chapter_index', models.IntegerField(null=True, verbose_name='chapter index', blank=True)),
+                ('viewer_playbackmode', models.CharField(max_length=32, null=True, verbose_name='Viewer Playback', blank=True)),
             ],
             options={
                 'abstract': False,
             },
-            bases=('base.resourcebase', geonode.utils.GXPMapBase),
+            bases=('maps.map',),
         ),
         migrations.CreateModel(
-            name='MapLayer',
+            name='MapStory',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('stack_order', models.IntegerField(verbose_name='stack order')),
-                ('format', models.CharField(max_length=200, null=True, verbose_name='format', blank=True)),
-                ('name', models.CharField(max_length=200, null=True, verbose_name='name')),
-                ('opacity', models.FloatField(default=1.0, verbose_name='opacity')),
-                ('styles', models.CharField(max_length=200, null=True, verbose_name='styles', blank=True)),
-                ('transparent', models.BooleanField(default=False, verbose_name='transparent')),
-                ('fixed', models.BooleanField(default=False, verbose_name='fixed')),
-                ('group', models.CharField(max_length=200, null=True, verbose_name='group', blank=True)),
-                ('visibility', models.BooleanField(default=True, verbose_name='visibility')),
-                ('ows_url', models.URLField(null=True, verbose_name='ows URL', blank=True)),
-                ('layer_params', models.TextField(verbose_name='layer params')),
-                ('source_params', models.TextField(verbose_name='source params')),
-                ('local', models.BooleanField(default=False)),
-                ('map', models.ForeignKey(related_name='layer_set', to='mapstories.Map')),
+                ('resourcebase_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='base.ResourceBase')),
+                ('distribution_url', models.TextField(help_text='information about on-line sources from which the dataset, specification, or community profile name and extended metadata elements can be obtained', null=True, verbose_name='distribution URL', blank=True)),
+                ('distribution_description', models.TextField(help_text='detailed text description of what the online resource is/does', null=True, verbose_name='distribution description', blank=True)),
             ],
             options={
-                'ordering': ['stack_order'],
+                'abstract': False,
+                'db_table': 'maps_mapstory',
+                'verbose_name_plural': 'MapStories',
             },
-            bases=(models.Model, geonode.utils.GXPLayerBase),
+            bases=('base.resourcebase',),
         ),
-        migrations.CreateModel(
-            name='MapSnapshot',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('config', models.TextField(verbose_name='JSON Configuration')),
-                ('created_dttm', models.DateTimeField(auto_now_add=True)),
-                ('map', models.ForeignKey(related_name='snapshot_set', to='mapstories.Map')),
-                ('user', models.ForeignKey(blank=True, to=settings.AUTH_USER_MODEL, null=True)),
-            ],
+        migrations.AddField(
+            model_name='map',
+            name='story',
+            field=models.ForeignKey(related_name='chapter_list', blank=True, to='mapstories.MapStory', null=True),
         ),
     ]
