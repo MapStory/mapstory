6e20baa1c78528f46e06a6a1570becc3e5011714
Tyler Battle <tbattle@boundlessgeo.com>
2017-04-19 13:11:43 -0700

    During test teardown, kill connections to the django database

--- mapstory/tests/__init__.py
+++ mapstory/tests/__init__.py
@@ -6,3 +6,7 @@ def tearDownPackage():
         FROM pg_stat_activity
         WHERE pg_stat_activity.datname = 'test_mapstory_geogig'
           AND pid <> pg_backend_pid();""")
+    cursor.execute("""SELECT pg_terminate_backend(pg_stat_activity.pid)
+        FROM pg_stat_activity
+        WHERE pg_stat_activity.datname = 'test_mapstory_data'
+          AND pid <> pg_backend_pid();""")
