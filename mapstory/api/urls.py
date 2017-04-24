e673ad68fad4f06d4d15884616cc8818e241af4b
Sara Safavi <sara@sarasafavi.com>
2017-04-24 12:10:20 -0500

    Register all of the API resources in our replacement for api/base

--- mapstory/api/urls.py
+++ mapstory/api/urls.py
@@ -1,7 +1,12 @@
-from resourcebase_api import MapStoryResource, ResourceBaseResource
-from api import MapstoryOwnersResource
+from mapstory.api.resourcebase_api import LayerResource, MapResource, DocumentResource, \
+    ResourceBaseResource, FeaturedResourceBaseResource, MapStoryResource
+from mapstory.api.api import MapstoryOwnersResource
 from geonode.api.urls import api
 
+api.register(LayerResource())
+api.register(MapResource())
+api.register(DocumentResource())
+api.register(ResourceBaseResource())
+api.register(FeaturedResourceBaseResource())
 api.register(MapStoryResource())
 api.register(MapstoryOwnersResource())
-api.register(ResourceBaseResource())
