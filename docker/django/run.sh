726e22e3304d2b8fb4123bc72583c0aa24b53323
Tyler Battle <tbattle@boundlessgeo.com>
2017-05-06 21:31:24 -0700

    Changes

--- docker/django/run.sh
+++ docker/django/run.sh
@@ -1,16 +1,32 @@
 #!/bin/sh
 set -e
 
+wait_for_pg()
+{
+    echo 'Waiting for Postgres...'
+    while ! curl http://$DATABASE_HOST:$DATABASE_PORT/ 2>&1 | grep '52'
+    do
+        sleep 1
+    done
+    echo 'Postgres is up'
+}
+
+# Reset permissions on the shared volumes
+# The nginx container seems to break these regularly
+#chown -R mapstory:mapstory $STATIC_ROOT
+#chown -R mapstory:mapstory $MEDIA_ROOT
+#chown -R mapstory:mapstory $APP_PATH/cover
+
 echo 'Testing permissions...'
-touch $MEDIA_ROOT/.ignore
-touch $STATIC_ROOT/.ignore
-touch $APP_PATH/mapstory/cover/.ignore
-rm $MEDIA_ROOT/.ignore
-rm $STATIC_ROOT/.ignore
-rm $APP_PATH/mapstory/cover/.ignore
+touch $MEDIA_ROOT/.ignore_$HOSTNAME
+touch $STATIC_ROOT/.ignore_$HOSTNAME
+touch $APP_PATH/cover/.ignore_$HOSTNAME
+rm $MEDIA_ROOT/.ignore_$HOSTNAME
+rm $STATIC_ROOT/.ignore_$HOSTNAME
+rm $APP_PATH/cover/.ignore_$HOSTNAME
 echo 'Permissions look good'
 
-cd $APP_PATH/mapstory
+cd $APP_PATH
 
 # Load secrets as environment variables
 for secret in /run/secrets/env_*; do
@@ -25,6 +41,7 @@ done
 for i do # loop over $@
 
     if [ "$i" = "--init-db" ]; then
+        wait_for_pg
         python manage.py migrate maps --noinput
         python manage.py migrate journal --noinput
         python manage.py migrate layers --noinput
@@ -40,11 +57,18 @@ for i do # loop over $@
         python manage.py collectstatic --noinput --ignore node_modules
     fi
 
+    if [ "$i" = "--collect-static-dev" ]; then
+        rm -rf $STATIC_ROOT/*
+        python manage.py collectstatic --link --noinput --ignore node_modules
+    fi
+
     if [ "$i" = "--reindex" ]; then
+        wait_for_pg
         python manage.py rebuild_index --noinput
     fi
 
     if [ "$i" = "--test" ]; then
+        wait_for_pg
         rm -f cover/*
         CELERY_ALWAYS_EAGER=true CELERY_EAGER_PROPAGATES_EXCEPTIONS=true coverage run ./manage.py test
         coverage report
@@ -56,19 +80,30 @@ for i do # loop over $@
     fi
 
     if [ "$i" = "--celery" ]; then
+        wait_for_pg
         celery -A mapstory worker -B -E -l info --concurrency=$CELERY_NUM_WORKERS
     fi
 
     if [ "$i" = "--serve" ]; then
+        wait_for_pg
         echo 'Running WSGI server'
         python /usr/local/bin/gunicorn \
-            --pythonpath=$APP_PATH/mapstory \
+            --pythonpath=$APP_PATH \
             --workers=4 \
             --timeout=120 \
-            --bind=0.0.0.0:8000 \
+            --bind=0.0.0.0:$DJANGO_PORT \
             --log-level=debug \
             --access-logfile=- \
             --error-logfile=- \
             mapstory.wsgi
+    fi
+
+    if [ "$i" = "--serve-dev" ]; then
+        wait_for_pg
+        echo 'Copying site-packages'
+        rm -rf /usr/local/lib/python2.7/site-packages-copy
+        cp -r /usr/local/lib/python2.7/site-packages /usr/local/lib/python2.7/site-packages-copy
+        echo 'Running dev server'
+        python manage.py runserver 0.0.0.0:$DJANGO_PORT
     fi
 done
