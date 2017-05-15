8dd437e9aff543e269f0aba0872b5a534ed37ec1
Zunware <jose@zunware.com>
2017-05-15 15:17:23 -0500

    Pep8 formatting

--- mapstory/tests/unit/test_utils.py
+++ mapstory/tests/unit/test_utils.py
@@ -7,35 +7,28 @@ from mapstory.utils import error_response, Link
 
 class TestUtils(MapStoryTestMixin):
 
-
     def setUp(self):
         pass
-
 
     def tearDown(self):
         pass
 
-
     def test_error_response(self):
-        statusCode = 111,
+        status_code = 111,
         text = "Test error 111"
 
-        error = error_response(statusCode, text)
+        error = error_response(status_code, text)
         self.assertIsInstance(error, HttpResponse)
-        self.assertEqual(error.status_code, statusCode)
-
+        self.assertEqual(error.status_code, status_code)
 
 
 class TestLinkUtil(TestCase):
 
-
     def setUp(self):
         pass
 
-
     def tearDown(self):
         pass
-
 
     def test_link(self):
         url = "https://josellausas.com"
@@ -46,7 +39,6 @@ class TestLinkUtil(TestCase):
         self.assertEqual(link.render(), '<a target="_" href="%s">%s</a>' % (url, name))
         self.assertFalse(link.is_image())
 
-
     def test_image_link(self):
         image_url = "http://tierranuestra.es/blog/wp-content/uploads/2014/12/image-4434489.jpg"
         name = "An Image Of Queso"
@@ -55,13 +47,11 @@ class TestLinkUtil(TestCase):
         self.assertEqual(link.render(), '<img class="None" src="%s" title="%s"></img>' % (image_url, name))
         self.assertEqual(link.name, name)
 
-
     def test_facebook_link(self):
         fb_url = "https://www.facebook.com/RaspberryPiPuebla"
         name = "Raspberry Pi Puebla"
         link = Link(fb_url, name)
         self.assertEqual(link.render(), '<a target="_" href="%s"><img src="/static/img/facebook.png" border=0></a>' % (fb_url,))
-
 
     def test_youtube_link(self):
         video_id = "8np2I_gQgcY"
@@ -70,6 +60,3 @@ class TestLinkUtil(TestCase):
         link = Link(video_link, name)
         self.assertTrue(video_id in link.render())
         self.assertEqual(name, link.name)
-
-
-
