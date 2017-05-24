155c1733d3cf33e2061bbf9ce58fc64e64b2b45a
Tyler Battle <tbattle@boundlessgeo.com>
2017-05-24 15:31:02 -0700

    Updates to the Django container’s run script

--- docker/django/run.sh
+++ docker/django/run.sh
@@ -21,9 +21,11 @@ echo 'Testing permissions...'
 touch $MEDIA_ROOT/.ignore_$HOSTNAME
 touch $STATIC_ROOT/.ignore_$HOSTNAME
 touch $APP_PATH/cover/.ignore_$HOSTNAME
+touch /usr/local/lib/python2.7/site-packages-copy/.ignore_$HOSTNAME
 rm $MEDIA_ROOT/.ignore_$HOSTNAME
 rm $STATIC_ROOT/.ignore_$HOSTNAME
 rm $APP_PATH/cover/.ignore_$HOSTNAME
+rm /usr/local/lib/python2.7/site-packages-copy/.ignore_$HOSTNAME
 echo 'Permissions look good'
 
 cd $APP_PATH
@@ -41,33 +43,40 @@ done
 for i do # loop over $@
 
     if [ "$i" = "--init-db" ]; then
+        echo 'Initializing database'
         wait_for_pg
-        python manage.py migrate maps --noinput
-        python manage.py migrate journal --noinput
-        python manage.py migrate layers --noinput
-        python manage.py migrate groups --noinput
-        python manage.py migrate --noinput
+        #python manage.py migrate maps --noinput
+        #python manage.py migrate journal --noinput
+        #python manage.py migrate layers --noinput
+        #python manage.py migrate groups --noinput
+        echo '    syncdb'
         python manage.py syncdb --noinput
+        echo '    migrate'
+        python manage.py migrate --noinput
 
         python manage.py create_admin_user --name=admin --password=admin
     fi
 
     if [ "$i" = "--collect-static" ]; then
+        echo 'Collecting static files'
         rm -rf $STATIC_ROOT/*
         python manage.py collectstatic --noinput --ignore node_modules
     fi
 
     if [ "$i" = "--collect-static-dev" ]; then
+        echo 'Collecting static files for dev deployment'
         rm -rf $STATIC_ROOT/*
         python manage.py collectstatic --link --noinput --ignore node_modules
     fi
 
     if [ "$i" = "--reindex" ]; then
+        echo 'Reindexing'
         wait_for_pg
         python manage.py rebuild_index --noinput
     fi
 
     if [ "$i" = "--test" ]; then
+        echo 'Running tests'
         wait_for_pg
         rm -f cover/*
         CELERY_ALWAYS_EAGER=true CELERY_EAGER_PROPAGATES_EXCEPTIONS=true coverage run ./manage.py test
@@ -80,11 +89,13 @@ for i do # loop over $@
     fi
 
     if [ "$i" = "--celery" ]; then
+        echo 'Running celery workers'
         wait_for_pg
         celery -A mapstory worker -B -E -l info --concurrency=$CELERY_NUM_WORKERS
     fi
 
     if [ "$i" = "--serve" ]; then
+        echo 'Serving...'
         wait_for_pg
         echo 'Running WSGI server'
         python /usr/local/bin/gunicorn \
@@ -99,10 +110,11 @@ for i do # loop over $@
     fi
 
     if [ "$i" = "--serve-dev" ]; then
+        echo 'Dev serving...'
         wait_for_pg
-        echo 'Copying site-packages'
-        rm -rf /usr/local/lib/python2.7/site-packages-copy
-        cp -r /usr/local/lib/python2.7/site-packages /usr/local/lib/python2.7/site-packages-copy
+        echo 'Copying site-packages' # served by nginx
+        rm -rf /usr/local/lib/python2.7/site-packages-copy/*
+        cp -r /usr/local/lib/python2.7/site-packages/* /usr/local/lib/python2.7/site-packages-copy/
         echo 'Running dev server'
         python manage.py runserver 0.0.0.0:$DJANGO_PORT
     fi
