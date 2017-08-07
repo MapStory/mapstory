2aea7ec4a1d3fe976ca2b281fdb9ca4ce22216ac
Coop56 <JCooper@jeremiahcooper.com>
2017-08-07 16:35:34 -0400

    Ensure that the slug is unique when saving stories

--- mapstory/mapstories/migrations/0002_mapstory_slug.py
+++ mapstory/mapstories/migrations/0002_mapstory_slug.py
@@ -14,7 +14,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='mapstory',
             name='slug',
-            field=models.SlugField(max_length=160, null=True),
-            preserve_default=False,
+            field=models.SlugField(max_length=160, unique=True, null=True),
         ),
     ]
