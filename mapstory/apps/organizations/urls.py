a6d903ad797eda59f7ce323a8a362238930e7819
Jose Llausas <jose@josellausas.com>
2017-08-29 18:00:05 -0500

    Added admin buttons to layer and mapstory cards

--- mapstory/apps/organizations/urls.py
+++ mapstory/apps/organizations/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     url(r'(?P<pk>\d+)/$', views.organization_detail, name='detail'),
     url(r'^$', views.organization_list, name='list'),
     url(r'(?P<pk>\d+)/add/layer/(?P<layer_pk>\d+)/$', views.add_layer, name='add_layer'),
-    url(r'(?P<pk>\d+)/add/mapstory/(?P<layer_pk>\d+)/$', views.add_mapstory, name='add_mapstory'),
-    url(r'(?P<pk>\d+)/add/member/(?P<layer_pk>\d+)/$', views.add_membership, name='add_member'),
+    url(r'(?P<pk>\d+)/add/mapstory/(?P<mapstory_pk>\d+)/$', views.add_mapstory, name='add_mapstory'),
+    url(r'(?P<pk>\d+)/add/member/(?P<user_pk>\d+)/$', views.add_membership, name='add_member'),
+    url(r'(?P<pk>\d+)/manager/$', views.manager, name='manage'),
 ]
--- mapstory/apps/organizations/views.py
+++ mapstory/apps/organizations/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse, HttpResponseForbidden
+from django.core.urlresolvers import reverse
+from django.contrib.auth import get_user_model
 
-from .models import Organization, OrganizationMembership, OrganizationURL, \
-    OrganizationLayer, OrganizationMapStory
+User = get_user_model()
+
+from .models import Organization, OrganizationMembership, OrganizationURL, OrganizationLayer, OrganizationMapStory
+
 
 def organization_detail(request, pk):
     """Organization Detail View.
@@ -19,32 +24,30 @@ def organization_detail(request, pk):
 
     layers = []
     mapstories = []
-    featured_stories = []
-    featured_layers = []
+
+    # Check membership
+    membership = None
+    memberships = OrganizationMembership.objects.filter(organization=org, user__pk=request.user.pk)
+    if memberships.count() > 0:
+        membership = memberships.first()
 
     for l in org_layers:
         share_url = "https://%s/layers/%s" % (request.get_host(), l.layer.name)
         layers.append({
             'layer': l.layer,
-            'url': share_url
+            'url': share_url,
+            'is_featured': l.is_featured
         })
-        if l.is_featured:
-            featured_layers.append({
-                'layer': l.layer,
-                'url': share_url
-            })
+
 
     for m in org_mapstories:
         share_url = "https://%s/story/%s" % (request.get_host(), m.mapstory.id)
         mapstories.append({
             'mapstory': m.mapstory,
-            'url': share_url
+            'url': share_url,
+            'is_featured': m.is_featured,
         })
-        if m.is_featured:
-            featured_stories.append({
-                'mapstory': m.mapstory,
-                'url': share_url
-            })
+
 
     context = {
         'org': org,
@@ -52,8 +55,7 @@ def organization_detail(request, pk):
         'urls': org_urls,
         'layers': layers,
         'mapstories': mapstories,
-        'featured_stories': featured_stories,
-        'featured_layers': featured_layers
+        'membership': membership
     }
 
     return render(request, 'organizations/organization_detail.html', context)
@@ -84,23 +86,53 @@ def membership_detail(request, org_pk, membership_pk):
     }
     return render(request, 'organizations/membership_detail.html', context)
 
-def add_layer(request, org_pk, layer_pk):
+def add_layer(request, pk, layer_pk):
     if request.POST:
         # TODO: Handle form POST
         # TODO: Check user's permissions
         pass
     return render(request, 'organizations/confirm_add.html', {})
 
-def add_mapstory(request, org_pk, mapstory_pk):
+def add_mapstory(request, pk, mapstory_pk):
     if request.POST:
         # TODO: Handle form POST
         # TODO: Check user's permissions
         pass
     return render(request, 'organizations/confirm_add.html', {})
 
-def add_membership(request, org_pk, user_pk):
+@login_required
+def add_membership(request, pk, user_pk):
+    # Check that we are admins
+    organization = get_object_or_404(Organization, pk=pk)
+    membership = get_object_or_404(OrganizationMembership, organization=organization, user=request.user)
+
+    if not membership.is_admin:
+        # User is NOT AUTHORIZED!
+        return HttpResponseForbidden()
+
     if request.POST:
-        # TODO: Handle form POST
-        # TODO: Check user's permissions
-        pass
-    return render(request, 'organizations/confirm_add.html', {})
+        # User is Authorized to add users to this organization
+        user_to_add = get_object_or_404(User, pk=user_pk)
+        membership = OrganizationMembership.objects.get_or_create(organization=organization, user=user_to_add)
+
+        return redirect(
+            reverse('organizations:member_detail', kwargs={
+                'org_pk': organization.pk,
+                'membership_pk':membership.pk}
+            )
+        )
+
+    return redirect(request, reverse('organizations:manager', kwargs={'pk':pk}))
+
+
+@login_required
+def manager(request, pk):
+    # Check that we are admins
+    organization = get_object_or_404(Organization, pk=pk)
+    membership = get_object_or_404(OrganizationMembership, organization=organization, user=request.user)
+
+    if not membership.is_admin:
+        # User is NOT AUTHORIZED!
+        return HttpResponseForbidden()
+
+    return render(request, 'organizations/manager.html', {'organization':organization})
