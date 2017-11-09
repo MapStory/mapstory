de9742426fa6f8924696ea316522d906412de655
Tyler Battle <tbattle@boundlessgeo.com>
2017-11-09 10:18:03 -0600

    Use Travis' Sauce connection if available

--- mapstory/tests/runAllTests.sh
+++ mapstory/tests/runAllTests.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 exit_status=0
 
+if [ $TRAVIS ]; then
+    SELENIUM="ondemand.saucelabs.com/wd/hub"
+else
+    SELENIUM="sauce-connect:4445/wd/hub"
+fi
+
 # Run end-to-end tests
 ./runE2ETests.sh; (( exit_status = exit_status || $? ))
 
 # Run acceptance tests
 pushd acceptance
-codeceptjs run --steps --override "{ \"helpers\": { \"Protractor\": { \"seleniumAddress\": \"http://$SAUCE_USERNAME:$SAUCE_ACCESS_KEY@sauce-connect:4445/wd/hub\"} } }"; (( exit_status = exit_status || $? ))
+codeceptjs run --steps --override "{ \"helpers\": { \"Protractor\": { \"seleniumAddress\": \"http://$SAUCE_USERNAME:$SAUCE_ACCESS_KEY@$SELENIUM\"} } }"; (( exit_status = exit_status || $? ))
 popd
 
 echo "Done running all tests."
