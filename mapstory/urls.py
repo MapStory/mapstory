79c40b7667544237c946a5b1976e1346934a44b2
Jose Llausas <jose@zunware.com>
2017-11-27 14:17:25 -0600

    Setup groupsapi urls.

--- mapstory/urls.py
+++ mapstory/urls.py
@@ -38,9 +38,10 @@ from mapstory.views import map_detail
 from mapstory.views import new_map
 from mapstory.views import ProfileDetail, profile_delete, profile_edit, proxy
 from mapstory.views import SearchView
-from mapstory.apps.organizations.views import organization_detail
+from mapstory.apps.organizations.api import OrganizationResource
 
 
+org_resource = OrganizationResource()
 geonode_api.register(ResourceBaseResource())
 geonode_api.register(MapstoryOwnersResource())
 geonode_api.register(InterestsResource())
@@ -143,6 +144,7 @@ urlpatterns = patterns('',
     url(r'^layers/resolve_user', resolve_user_mapstory, name='resolve_user_mapstory'),
     url(r'^organizations/', include('mapstory.apps.organizations.urls', namespace='organizations')),
     url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type="text/plain"), name='robots'),
+    url(r'^groupsapi/', include(org_resource.urls))
 ) + geonode_layers_urlpatterns + layer_detail_patterns + urlpatterns
 
 urlpatterns += annotations_urls
