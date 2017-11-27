869e1e18712122e2f84f4e8d3a7020558ff6377e
Jose Llausas <jose@zunware.com>
2017-11-27 14:17:25 -0600

    Setup groupsapi urls.

---
+++ mapstory/apps/organizations/api.py
@@ -0,0 +1,8 @@
+from tastypie.resources import ModelResource
+from .models import Organization
+
+
+class OrganizationResource(ModelResource):
+    class Meta:
+        queryset = Organization.objects.all()
+        resource_name = 'organization'
--- mapstory/apps/organizations/urls.py
+++ mapstory/apps/organizations/urls.py
@@ -1,8 +1,13 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 
 from . import views
+from . import api
+
+organization_resource = api.OrganizationResource()
+
 urlpatterns = [
     url(r'^$', views.organization_list, name='list'),
+    url(r'^api/', include(organization_resource.urls)),
     url(r'(?P<slug>[-\w]+)/add/layer/(?P<layer_pk>\d+)/$', views.add_layer, name='add_layer'),
     url(r'(?P<slug>[-\w]+)/add/mapstory/(?P<mapstory_pk>\d+)/$', views.add_mapstory, name='add_mapstory'),
     url(r'(?P<slug>[-\w]+)/join/$', views.request_membership, name='request_membership'),
