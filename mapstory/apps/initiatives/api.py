47a8cac20b7acf345f1df3a8647a548c74393082
Jose Llausas <jose@zunware.com>
2017-12-08 15:28:12 -0600

    Added filters.

--- mapstory/apps/initiatives/api.py
+++ mapstory/apps/initiatives/api.py
@@ -1,4 +1,6 @@
+from tastypie.constants import ALL
 from tastypie.resources import ModelResource
+
 from .models import Initiative
 
 
@@ -6,3 +8,16 @@ class InitiativeResource(ModelResource):
     class Meta:
         queryset = Initiative.objects.all()
         resource_name = 'initiative'
+        allowed_methods = ['get']
+
+        filtering = {
+            'title': ALL,
+            'city': ALL,
+            'country': ALL,
+            'is_active': ALL,
+        }
+
+    def dehydrate(self, bundle):
+        bundle.data['url'] = bundle.obj.get_absolute_url()
+        bundle.data['members'] = bundle.obj.get_member_count()
+        return bundle
