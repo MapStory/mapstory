c5d60d53ea6cf9166b969f9684e8b4abf96901b6
Jose Llausas <jose@zunware.com>
2017-12-11 12:08:13 -0600

    Making progress

--- mapstory/apps/base_groups/tests.py
+++ mapstory/apps/base_groups/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 
+from mapstory.apps.organizations.models import Organization
 
 class BaseGroupTests(TestCase):
     def setUp(self):
@@ -12,3 +13,9 @@ class BaseGroupTests(TestCase):
     def test_api_urls(self):
         response = self.client.get('/groupsapi/api/group', follow=True)
         self.assertEqual(200, response.status_code)
+
+    def test_name_query(self):
+        org = Organization.objects.create(name='test', slogan='123', about='456')
+        response = self.client.get('/search/?groups&limit=30&offset=0&name=test')
+        self.assertEqual(200, response.status_code)
+        self.assertContains(response, 'test')
