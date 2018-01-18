21d999807604c0dde562c1cc84eb8607c30eba8b
Jeremiah Cooper <JCooper@jeremiahcooper.com>
2018-01-18 16:18:18 -0500

    Change the test script to run all tests and check coverage, not just the ones in the apps directory.

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-coverage run --branch --source=mapstory.apps ./manage.py test # && \
+coverage run --branch --source=mapstory ./manage.py test # && \
 coverage report # && \
 coverage html -d cover
