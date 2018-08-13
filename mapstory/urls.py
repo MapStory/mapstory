e30c1f680242ae6d3559485b887030e4a8bd7dc5
James Meyer <jmeyer@boundlessgeo.com>
2018-08-13 11:22:10 -0400

    Added ability to embed story layers

--- mapstory/urls.py
+++ mapstory/urls.py
@@ -117,6 +117,7 @@ urlpatterns = patterns('',
                        url(r'^layers/resolve_user', resolve_user_mapstory, name='resolve_user_mapstory'),
                        url(r'^layers/(?P<layername>[^/]*)$', layer_detail, name="layer_detail"),
                        url(r'^layers/(?P<layername>[^/]*)/viewer$', layer_detail, {'template': 'viewer/layer_viewer.html'}, name="layer_viewer"),
+                       url(r'^layers/(?P<layername>[^/]*)/embed$', layer_detail, {'template': 'viewer/layer_viewer.html'}, name="layer_embed"),
                        url(r'^layers/(?P<layername>[^/]*)/remove$', layer_remove, name="layer_remove"),
                        url(r'^layers/(?P<layername>[^/]*)/remote$', get_remote_url, name="get_remote_url"),
 
