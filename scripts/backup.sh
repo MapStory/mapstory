d5fa6db0213255469d1366a4bad94a745a661328
Tyler Battle <tbattle@boundlessgeo.com>
2018-09-06 14:18:59 -0700

    Backup shell script needs to be run in interactive mode.

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
+#!/bin/bash -i
+# Interactive mode bash is used so that the alias "dco" functions properly.
 
 set -ex
+
+cd /home/admin/mapstory # Expects to be run on the prod deployment
 
 # Backup Postgres DBs
 dco run --rm pgadmin --dump
