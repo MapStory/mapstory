34ef70aeba25cf6d20f97b0ad9ac855a04b5b45c
Jeremiah Cooper <JCooper@jeremiahcooper.com>
2018-09-12 17:29:44 -0400

    Moves the basic storyframes models and admin section from the old storyframes app to the mapstories app.

--- mapstory/mapstories/admin.py
+++ mapstory/mapstories/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 
+from mapstory.mapstories.models import StoryFrame
 from mapstory.mapstories.models import StoryPin
+
+
+class StoryFrameAdmin(admin.ModelAdmin):
+    list_display = ('id', 'map', 'title')
+    list_filter = ('map',)
+    search_fields = ('map__title', 'title', 'description',)
 
 
 class StoryPinAdmin(admin.ModelAdmin):
@@ -8,5 +15,6 @@ class StoryPinAdmin(admin.ModelAdmin):
     list_filter = ('map', 'in_map', 'in_timeline',)
     search_fields = ('map__title', 'title', 'content',)
 
-
+ 
+admin.site.register(StoryFrame, StoryFrameAdmin)
 admin.site.register(StoryPin, StoryPinAdmin)
--- mapstory/mapstories/models.py
+++ mapstory/mapstories/models.py
@@ -212,6 +212,65 @@ def default_is_published(sender, **kwargs):
         instance.is_published = False
 
 
+class StoryFrameManager(models.Manager):
+
+    def copy_map_story_frames(self, source_id, target):
+        source = Map.objects.get(id=source_id)
+        copies = []
+
+        for storyframe in source.storyframe_set.all():
+            storyframe.map = target
+            storyframe.pk = None
+            copies.append(storyframe)
+
+        StoryFrame.objects.bulk_create(copies)
+
+
+class StoryFrame(models.Model):
+    objects = StoryFrameManager()
+
+    PLAYBACK_RATE = (('seconds', 'Seconds'),('minutes', 'Minutes'),)
+    INTERVAL_RATE = (('minutes', 'Minutes'),('hours', 'Hours'),
+                     ('weeks', 'Weeks'),('months', 'Months'),('years', 'Years'),)
+
+    map = models.ForeignKey(Map)
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    the_geom = models.TextField(blank=True, null=True)
+    start_time = models.BigIntegerField(blank=True, null=True)
+    end_time = models.BigIntegerField(blank=True, null=True)
+    data = models.TextField(blank=True, null=True)
+    center = models.TextField(blank=True, null=True)
+    interval = models.IntegerField(blank=True, null=True)
+    intervalRate = models.CharField(max_length=10, choices=INTERVAL_RATE, blank=True, null=True)
+    playback = models.IntegerField(blank=True, null=True)
+    playbackRate = models.CharField(max_length=10, choices=PLAYBACK_RATE, blank=True, null=True)
+    speed = models.TextField(blank=True, null=True)
+    zoom = models.IntegerField(blank=True, null=True)
+    layers = models.TextField(blank=True, null=True)
+    resolution = models.TextField(blank=True, null=True)
+
+    def _timefmt(self, val):
+        return datetime.isoformat(datetime.utcfromtimestamp(val))
+
+    def set_start(self, val):
+        self.start_time = parse_date_time(val)
+
+    def set_end(self, val):
+        self.end_time = parse_date_time(val)
+
+    @property
+    def start_time_str(self):
+        return self._timefmt(self.start_time) if self.start_time else ''
+
+    @property
+    def end_time_str(self):
+        return self._timefmt(self.end_time) if self.end_time else ''
+
+    class Meta:
+        verbose_name_plural = "StoryFrame"
+
+
 class StoryPinManager(models.Manager):
 
     def copy_map_storypins(self, source_id, target):
@@ -257,13 +316,6 @@ class StoryPin(models.Model):
     def end_time_str(self):
         return self._timefmt(self.end_time) if self.end_time else ''
 
-
-def map_copied(sender, source_id, **kw):
-    try:
-        StoryPin.objects.copy_map_storypins(source_id, sender)
-    except:
-        import traceback
-        traceback.print_exc()
 
 signals.post_init.connect(default_is_published, sender=MapStory)
 signals.post_init.connect(default_is_published, sender=Map)
