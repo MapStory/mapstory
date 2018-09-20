7c230f227bde18d6cdfb973abb9082adaa621861
Jeremiah Cooper <JCooper@jeremiahcooper.com>
2018-09-20 18:23:46 -0400

    WIP.  Basic StoryPin saving is now functional, need to finish saving all of the fields and ensure that duplicate pins aren't created.

--- mapstory/mapstories/views.py
+++ mapstory/mapstories/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 
 from .models import Map
 from .models import MapStory
+from .models import StoryPin
 from geonode.people.models import Profile
 
 
@@ -14,8 +15,10 @@ def save_mapstory(request):
 
     if config['storyID']:
         mapstory = MapStory.objects.get(pk=config['storyID'])
+        print("Found a mapstory, it is", mapstory)
     else:
         mapstory = MapStory(owner=request.user)
+        print("There was no MapStory, making a new one.", mapstory)
 
     mapstory.save()
     config['storyID'] = mapstory.id
@@ -23,7 +26,7 @@ def save_mapstory(request):
 
     for index, chapter in enumerate(config['chapters']):
 
-        config['chapters'][index]["storyID"] = mapstory.id
+        config['chapters'][index]['storyID'] = mapstory.id
 
         if config['chapters'][index]['mapId']:
             currentChapter = Map.objects.get(pk=config['chapters'][index]['mapId'])
@@ -34,6 +37,28 @@ def save_mapstory(request):
         currentChapter.update_from_viewer(conf=chapter)
         config['chapters'][index]['mapId'] = currentChapter.id
 
+        if chapter['pins']['features']:
+            for index, pin in enumerate(chapter['pins']['features']):
+                print(pin)
+                if pin['id']:
+                    currentPin = StoryPin.objects.get(pk=pin['id'])
+                else:
+                    print("PIN ID IS NULL")
+                    currentPin = StoryPin()
+
+                currentPin.map_id = chapter['mapId']
+
+                #currentPin.auto_show = pin['properties']['autoShow']
+                currentPin.content = pin['properties']['content']
+                #currentPin.end_time = pin['properties']['end_time']
+                currentPin.in_map = pin['properties']['inMap']
+                currentPin.in_timeline = pin['properties']['inTimeline']
+                currentPin.media = pin['properties']['media']
+                #currentPin.start_time = pin['properties']['start_time']
+                currentPin.title = pin['properties']['title']
+
+                currentPin.save()
+                pin['id'] = currentPin.id
 
     return HttpResponse(json.dumps(config))
 
