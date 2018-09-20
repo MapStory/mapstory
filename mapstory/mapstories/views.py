1a4bcaf2e5219fb97f04da3d3aba82135150e9c2
Jeremiah Cooper <JCooper@jeremiahcooper.com>
2018-09-20 09:56:56 -0400

    WIP saving, this commit allows you to save basic mapstories with minimal information.

--- mapstory/mapstories/views.py
+++ mapstory/mapstories/views.py
@@ -1,21 +1,34 @@
 import json
 import requests
+
+from django.http import HttpResponse
+
 from .models import Map
 from .models import MapStory
+from geonode.people.models import Profile
 
 
 def save_mapstory(request):
-    print request.POST
     config = json.loads(request.body)
     print config
-    
-    mapstory = MapStory.objects.get(id=config['story_id'])
 
-    mapstory.title = config['about']['title']
-    mapstory.abstract = config['about']['abstract']
+    owner = Profile.objects.get(username=request.user)
 
-    for index, chapter in enumerate(config['chapters']):
-        map = Map.objects.get(id=config['chapters'][index]['map_id'])
-        map.update_from_viewer(chapter)
+    if config['storyId']:
+        mapstory = MapStory(pk=config['storyId'], owner=owner)
+    else:
+        mapstory = MapStory(owner=owner)
 
     mapstory.save()
+    
+    mapstory.update_from_viewer(conf=config)
+
+    # for index, chapter in enumerate(config['chapters']):
+    #     map = Map.objects.get(pk=config['chapters'][index]['mapId'])
+    #     map.update_from_viewer(chapter)
+
+    config['storyId'] = mapstory.id
+
+    return HttpResponse(json.dumps(config))
+
+
