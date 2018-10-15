71c2adde1155d87ba4d8e073f01df05c0202f3a0
Jeremiah Cooper <JCooper@jeremiahcooper.com>
2018-10-15 17:47:10 -0400

    Format code and order imports based on PEP8 for the API app.

--- mapstory/api/urls.py
+++ mapstory/api/urls.py
@@ -1,9 +1,10 @@
 from geonode.api.urls import api
-
 from mapstory.api.api import MapstoryOwnersResource
-from mapstory.api.resourcebase_api import LayerResource, MapResource, DocumentResource, \
-    ResourceBaseResource, FeaturedResourceBaseResource, MapStoryResource
-
+from mapstory.api.resourcebase_api import (DocumentResource,
+                                           FeaturedResourceBaseResource,
+                                           LayerResource, MapResource,
+                                           MapStoryResource,
+                                           ResourceBaseResource)
 
 api.register(LayerResource())
 api.register(MapResource())
