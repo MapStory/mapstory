bcce2fbeedbb7dd06426f7006597c9e8d58df495
James Meyer <jmeyer@boundlessgeo.com>
2019-01-16 14:29:49 -0500

    Fixes editing layers

--- mapstory/views.py
+++ mapstory/views.py
@@ -13,7 +13,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.conf import settings
 
-
 class IndexView(TemplateView):
     template_name = 'index.html'
 
@@ -76,11 +75,9 @@ def maploom_new_map(request, template='maps/map_new.html'):
 
     sources_array.append({
         "title": "GeoServer - Public Layers",
-        # "attribution": "&copy; %s" % SITEURL,
         "attribution": "&copy; %s",
         "ptype": "gxp_wmscsource",
-        # "url": OGC_SERVER['default']['PUBLIC_LOCATION'] + "wws",
-        "url": "https://docker/geoserver/wms",
+        "url": settings.SITEURL + "/geoserver/wms",
         "restUrl": "/gs/rest"
     })
     viewed_layer = config_obj["map"]["layers"][-1]
