8737c0a72ae444a8be438ed43c2a090995b8c5fc
Jeremiah Cooper <JCooper@jeremiahcooper.com>
2019-02-11 18:03:41 -0500

    Sets up chip based autocomplete on the main search field.

--- mapstory/static/mapstory/js/src/search/dataservice.factory.js
+++ mapstory/static/mapstory/js/src/search/dataservice.factory.js
@@ -1,16 +1,13 @@
- /*
-  */
+/*
+ */
 (function() {
+  angular.module("mapstory.search").factory("dataService", dataService);
 
-
-  angular
-    .module('mapstory.search')
-    .factory('dataService', dataService);
-
-  dataService.$inject = ['$http'];
+  dataService.$inject = ["$http"];
 
   function dataService($http) {
     return {
+      getSearchData,
       getKeywords,
       getOwners,
       getInterests,
@@ -20,41 +17,51 @@
 
     // //////////////////////
     function getInterests(query) {
-      return $http.get(INTERESTS_ENDPOINT,  {params: query || {}})
-           .then((response) => response.data.objects);
+      return $http
+        .get(INTERESTS_ENDPOINT, { params: query || {} })
+        .then(response => response.data.objects);
     }
 
     function getKeywords(query) {
-      return $http.get(KEYWORDS_ENDPOINT,  {params: query || {}})
-           .then((response) => response.data.objects);
+      return $http
+        .get(KEYWORDS_ENDPOINT, { params: query || {} })
+        .then(response => response.data.objects);
     }
 
-    function getOwners(query){
-      return $http.get('/api/owners/', {params: query || {}})
-          .then((response) => response.data.objects);
+    function getSearchData(query) {
+      return $http
+        .get("/api/base/search", { params: query || {} })
+        .then(response => response.data.objects);
+    }
+
+    function getOwners(query) {
+      return $http
+        .get("/api/owners/", { params: query || {} })
+        .then(response => response.data.objects);
     }
 
     // / regions needs to be updated to async
 
-    function getRegions(){
-      return $http.get(`${REGIONS_ENDPOINT  }?limit=500`)
-              .then(regionsForAutocomplete);
+    function getRegions() {
+      return $http
+        .get(`${REGIONS_ENDPOINT}?limit=500`)
+        .then(regionsForAutocomplete);
     }
 
     function regionsForAutocomplete(response) {
       const results = response.data.objects;
       const codes = {};
 
-      const countryResults = _.map(results, (region) => {
-          region._lower = [ region.name.toLowerCase() , region.code.toLowerCase() ];
-          codes[region.code] = region;
-          return region;
+      const countryResults = _.map(results, region => {
+        region._lower = [region.name.toLowerCase(), region.code.toLowerCase()];
+        codes[region.code] = region;
+        return region;
       });
 
       return {
         all: countryResults,
-        byCodes: codes,
-      }
+        byCodes: codes
+      };
     }
-  };
+  }
 })();
