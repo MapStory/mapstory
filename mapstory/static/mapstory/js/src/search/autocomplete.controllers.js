3c97a94251214563360ffe66c630b6ad256c4ded
Jeremiah Cooper <JCooper@jeremiahcooper.com>
2019-02-11 18:03:41 -0500

    Sets up chip based autocomplete on the main search field.

--- mapstory/static/mapstory/js/src/search/autocomplete.controllers.js
+++ mapstory/static/mapstory/js/src/search/autocomplete.controllers.js
@@ -1,112 +1,155 @@
-
 (function() {
-
   angular
-      .module('mapstory.search')
-      .controller('interestsController', interestsController)
-      .controller('tagsController', tagsController)
-      .controller('citiesController', citiesController)
-      .controller('storytellerController', storytellerController)
-      .controller('countriesController', countriesController);
-/*
-* Interests Autocomplete Controller
-*/
-  function interestsController ($injector, $scope, chipFieldFactory, dataService){
+    .module("mapstory.search")
+    .controller("interestsController", interestsController)
+    .controller("tagsController", tagsController)
+    .controller("citiesController", citiesController)
+    .controller("searchController", searchController)
+    .controller("storytellerController", storytellerController)
+    .controller("countriesController", countriesController);
+  /*
+   * Interests Autocomplete Controller
+   */
+  function interestsController(
+    $injector,
+    $scope,
+    chipFieldFactory,
+    dataService
+  ) {
     const vm = this;
-    vm.interests = new chipFieldFactory('interests', 'slug');
+    vm.interests = new chipFieldFactory("interests", "slug");
     vm.async = function(searchText) {
-       return dataService.getInterests({slug__icontains: searchText})
+      return dataService.getInterests({ slug__icontains: searchText });
     };
 
-    function interestChipSync(){
+    function interestChipSync() {
       const currentQuery = vm.interests.tidy($scope.query);
       // updates countryChips model with tidy, transformed list of actively selected countries
-      vm.chips = vm.interests.transform(currentQuery); 
+      vm.chips = vm.interests.transform(currentQuery);
       vm.disabled = vm.chips.length >= 1;
-    } 
+    }
 
-    $scope.$on('updateSelection', () => { 
-      interestChipSync(); 
+    $scope.$on("updateSelection", () => {
+      interestChipSync();
     });
 
     interestChipSync();
   }
 
-/*
-* Tag / Keyword Autocomplete Controller
-*/
-  function tagsController ($injector, $scope, chipFieldFactory, dataService){
+  /*
+   * Tag / Keyword Autocomplete Controller
+   */
+  function tagsController($injector, $scope, chipFieldFactory, dataService) {
     const vm = this;
-    vm.field = new chipFieldFactory('keywords__slug__in', 'slug');
+    vm.field = new chipFieldFactory("keywords__slug__in", "slug");
     vm.async = function(searchText) {
-      return dataService.getKeywords({slug__icontains: searchText})
-    }; 
+      return dataService.getKeywords({ slug__icontains: searchText });
+    };
 
-    function tagChipSync(){
+    function tagChipSync() {
       const currentQuery = vm.field.tidy($scope.query);
       vm.chips = vm.field.transform(currentQuery);
-      vm.placeholder = vm.chips.length > 0 ? "Expand your Tag Filter..." : "Filter by tag..."; 
+      vm.placeholder =
+        vm.chips.length > 0 ? "Expand your Tag Filter..." : "Filter by tag...";
     }
 
-    $scope.$on('updateSelection', () => { 
-      tagChipSync(); 
+    $scope.$on("updateSelection", () => {
+      tagChipSync();
     });
 
-    tagChipSync(); 
+    tagChipSync();
   }
 
-/*
-* Cities Autocomplete Controller
-*/
-  function citiesController ($injector, $scope, chipFieldFactory, dataService){
+  /*
+   * Cities Autocomplete Controller
+   */
+  function citiesController($injector, $scope, chipFieldFactory, dataService) {
     const vm = this;
-    vm.cities = new chipFieldFactory('city__icontains', 'city');
+    vm.cities = new chipFieldFactory("city__icontains", "city");
     vm.async = function(searchText) {
-      return dataService.getOwners({city__icontains: searchText})
+      return dataService.getOwners({ city__icontains: searchText });
     };
 
-    function cityChipSync(){
+    function cityChipSync() {
       const currentQuery = vm.cities.tidy($scope.query);
-      vm.cityChips = vm.cities.transform(currentQuery); 
-      if(vm.cityChips) vm.disabled = vm.cityChips.length >= 1;
+      vm.cityChips = vm.cities.transform(currentQuery);
+      if (vm.cityChips) vm.disabled = vm.cityChips.length >= 1;
     }
 
-    $scope.$on('updateSelection', () => {
-     cityChipSync(); 
+    $scope.$on("updateSelection", () => {
+      cityChipSync();
     });
 
-    cityChipSync(); 
+    cityChipSync();
   }
-
-/*
-* StoryTeller Autocomplete Controller
-*/
-  function storytellerController ($injector, $scope, chipFieldFactory, dataService){
+  /*
+   * Search Autocomplete Controller
+   */
+  function searchController($injector, $scope, chipFieldFactory, dataService) {
     const vm = this;
-    vm.author = new chipFieldFactory('owner__username__in', 'username');
+    vm.search = new chipFieldFactory("title__icontains", "title");
     vm.async = function(searchText) {
-      return dataService.getOwners({q: searchText});
+      return dataService.getSearchData({ q: searchText });
     };
 
-    function authorChipSync(){
+    function searchChipSync() {
+      const currentQuery = vm.search.tidy($scope.query);
+      vm.searchChips = vm.search.transform(currentQuery);
+      vm.placeholder =
+        vm.searchChips.length > 0
+          ? "Search for layers or stories..."
+          : "Search for layers or stories...";
+    }
+
+    $scope.$on("updateSelection", () => {
+      searchChipSync();
+    });
+
+    searchChipSync();
+  }
+
+  /*
+   * StoryTeller Autocomplete Controller
+   */
+  function storytellerController(
+    $injector,
+    $scope,
+    chipFieldFactory,
+    dataService
+  ) {
+    const vm = this;
+    vm.author = new chipFieldFactory("owner__username__in", "username");
+    vm.async = function(searchText) {
+      return dataService.getOwners({ q: searchText });
+    };
+
+    function authorChipSync() {
       const currentQuery = vm.author.tidy($scope.query);
       vm.userChips = vm.author.transform(currentQuery);
-      vm.placeholder = vm.userChips.length > 0 ? "Expand your StoryTeller Filter..." : "Filter by StoryTeller..."; 
+      vm.placeholder =
+        vm.userChips.length > 0
+          ? "Expand your StoryTeller Filter..."
+          : "Filter by StoryTeller...";
     }
 
-    $scope.$on('updateSelection', () => { 
-      authorChipSync(); 
+    $scope.$on("updateSelection", () => {
+      authorChipSync();
     });
 
     authorChipSync();
   }
-/*
-* Country Autocomplete Controller (TODO: need to update to async with code+name query)
-*/
+  /*
+   * Country Autocomplete Controller (TODO: need to update to async with code+name query)
+   */
 
-function countriesController ($injector, $scope, chipFieldFactory, dataService){
+  function countriesController(
+    $injector,
+    $scope,
+    chipFieldFactory,
+    dataService
+  ) {
     const vm = this;
-    vm.country = new chipFieldFactory('country');
+    vm.country = new chipFieldFactory("country");
 
     function querySearch(query) {
       const list = this.list;
@@ -115,53 +158,53 @@ function countriesController ($injector, $scope, chipFieldFactory, dataService){
     }
 
     function createFilter(query) {
-      const lowercaseQuery = query.toLowerCase();    
+      const lowercaseQuery = query.toLowerCase();
       return function filterFn(entry) {
-        return _.some(entry._lower, (i) => i.indexOf(lowercaseQuery) > -1);
+        return _.some(entry._lower, i => i.indexOf(lowercaseQuery) > -1);
       };
     }
     vm.country.querySearch = querySearch;
     vm.country.createFilter = createFilter;
     vm.countryChips = [];
-   
-    vm.country.transform = function(){
+
+    vm.country.transform = function() {
       // tidy any country filters on scope into a single array
       const currentQuery = vm.country.tidy($scope.query);
-      
+
       // transforms each query entry into country chip
-      return _.map(currentQuery, (item) => vm.country.byCodes[item]) || [];
-    }
+      return _.map(currentQuery, item => vm.country.byCodes[item]) || [];
+    };
 
     // / get the things, sync chips with query on delete ///
 
-    $scope.$on('updateSelection', () => { 
-      countryChipSync(); 
+    $scope.$on("updateSelection", () => {
+      countryChipSync();
     });
 
-    if( !$scope.autocomplete.countries ){
-      // if explore controller doesn't have list of regions / countries yet, 
+    if (!$scope.autocomplete.countries) {
+      // if explore controller doesn't have list of regions / countries yet,
       // get it from Region API
-      dataService.getRegions().then((data) => {
+      dataService.getRegions().then(data => {
         setAutocomplete(data);
-      });    
+      });
     } else {
       // else initialize autocomplete with list from explore controller
       setAutocomplete($scope.autocomplete.countries);
     }
 
-    function setAutocomplete (info) {
+    function setAutocomplete(info) {
       // store response in exploreController scope
       $scope.autocomplete.countries = info;
       // extend chipField object w/ list of regions and an index by country code
-        vm.country.byCodes = info.byCodes;
-        vm.country.list = info.all; 
+      vm.country.byCodes = info.byCodes;
+      vm.country.list = info.all;
       // update countryChips model
-      countryChipSync()
+      countryChipSync();
     }
 
-    function countryChipSync(){
+    function countryChipSync() {
       // updates countryChips model with tidy, transformed list of actively selected countries
-      if(vm.country.byCodes) vm.countryChips = vm.country.transform(); 
+      if (vm.country.byCodes) vm.countryChips = vm.country.transform();
 
       vm.disabled = vm.countryChips.length >= 1;
     }
